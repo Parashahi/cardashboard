287e096cfee0f2ef98efac6f5d4e5b7bb49fb2b7
Parashahi <parash.shahi777@gmail.com>
2022-10-15 23:35:13 +0545

    SaleReportPaper completed

--- src/components/core/SaleReportPaper.js
+++ src/components/core/SaleReportPaper.js
@@ -12,100 +12,133 @@ import img from "../../assets/images/Index";
 
 function SaleReportPaper() {
   const SalesReport = styled.div`
+    background: white;
     border: 1px solid lightgrey;
     border-radius: 5px;
-    width: 65%;
-    padding: 15px 20px;
+    width: 70%;
+    padding: 20px;
 
     .head {
+      align-items: center;
+      height: 30px;
       display: flex;
-      // justify-content: space-between;
+      width: 100%;
+      justify-content: space-between;
 
       p {
         font-weight: bold;
-        // margin-right: 40px;
-        font-size: 14px;
-        padding: 3px 50px 0 0;
+        font-size: 13px;
+        ${"" /* padding: 5px 20px 0 0; */}
       }
+      .radioGroup {
+        height: 25px;
+        display: flex;
+        gap: 5px;
+        align-items: center;
 
-      // .radioGroup {
-      .radioBtn {
-        margin: 0 2px;
-        border-radius: 5px;
-        position: relative;
-        text-align: center;
-        font-size: 12px;
-        font-weight: 600;
-        color: grey;
-
-        input {
-          opacity: 0;
-          width: 70px;
-
-          &:hover + .selected {
-            border: 1px solid lightgrey;
-            border-radius: 5px;
-            top: -1px;
-          }
-
-          &:checked + .selected {
-            border: 1px solid lightgrey;
-            border-radius: 5px;
-            top: -1px;
-            color: black;
-          }
-        }
-        .selected {
+        .radioBtn {
+          height: inherit;
+          position: relative;
+          text-align: center;
+          font-size: 10px;
+          font-weight: 600;
+          color: grey;
           box-sizing: border-box;
-          padding: 5px 0 4px;
-          width: 100%;
-          position: absolute;
-          left: 0;
-          top: 0;
+
+          input {
+            opacity: 0;
+            width: 80px;
+            height: inherit;
+
+            &:hover + .selected {
+              border: 1px solid lightgrey;
+              box-sizing: border-box;
+              border-radius: 5px;
+              padding: 6px 0 5px;
+              top: -1px;
+              left: 0;
+            }
+
+            &:checked + .selected {
+              border: 1px solid lightgrey;
+              border-radius: 5px;
+              padding: 6px 0 5px;
+              top: -1px;
+              color: black;
+            }
+          }
+          .selected {
+            box-sizing: border-box;
+            padding: 6px 0 5px;
+            width: 100%;
+            position: absolute;
+            left: 0;
+            top: 0px;
+          }
         }
       }
-      // }
 
       button {
+        font-size: 10px;
         font-weight: 600;
-        margin-left: 40px;
       }
     }
 
     .chart {
+      position: relative;
       width: 100%;
+
+      .lines {
+        width: 100%;
+        margin: 30px 0;
+        height: 1px;
+        background: lightgrey;
+      }
+
       img {
-        width: 565px;
-        height: 200px;
+        position: absolute;
+        top: -30px;
+        width: 100%;
+        height: 160px;
+      }
+
+      ul {
+        padding: 20px 0 0;
+        font-size: 12px;
+        color: grey;
+        display: flex;
+        justify-content: space-between;
       }
     }
   `;
   return (
     <SalesReport>
       <div className="head">
+        {/* <div className="title"> */}
         <p>Sales Report</p>
-
-        {/* <div className="radioGroup"> */}
-        <label className="radioBtn">
-          <input type="radio" value={1} name="a" />
-          <span className="selected">12 Months</span>
-        </label>
-
-        <label className="radioBtn">
-          <input type="radio" value={1} name="a" />
-          <span className="selected">6 Months</span>
-        </label>
-
-        <label className="radioBtn">
-          <input type="radio" value={1} name="a" />
-          <span className="selected">30 Days</span>
-        </label>
-
-        <label className="radioBtn">
-          <input type="radio" value={1} name="a" />
-          <span className="selected">7 Days</span>
-        </label>
         {/* </div> */}
+
+        <div className="radioGroup">
+          <label className="radioBtn">
+            <input type="radio" value={1} name="a" />
+            <span className="selected">12 Months</span>
+          </label>
+
+          <label className="radioBtn">
+            <input type="radio" value={1} name="a" />
+            <span className="selected">6 Months</span>
+          </label>
+
+          <label className="radioBtn">
+            <input type="radio" value={1} name="a" />
+            <span className="selected">30 Days</span>
+          </label>
+
+          <label className="radioBtn">
+            <input type="radio" value={1} name="a" />
+            <span className="selected">7 Days</span>
+          </label>
+        </div>
 
         <RectangleMedBtn
           icon={icon.ReportIcon}
@@ -115,7 +148,25 @@ function SaleReportPaper() {
       </div>
 
       <div className="chart">
+        <div className="lines"></div>
+        <div className="lines"></div>
+        <div className="lines"></div>
+        <div className="lines"></div>
         <img src={img.ChartImg} alt="" />
+        <ul>
+          <li>Jan</li>
+          <li>Feb</li>
+          <li>Mar</li>
+          <li>Apr</li>
+          <li>May</li>
+          <li>Jun</li>
+          <li>Jul</li>
+          <li>Aug</li>
+          <li>Sep</li>
+          <li>Oct</li>
+          <li>Nov</li>
+          <li>Dec</li>
+        </ul>
         {/* <Chart
           type="line"
           data={{
